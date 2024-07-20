45bbf956ad9c1dc67f85a3a49117835d66b34d6c
Arsal Azeem <36434007+arsalazeem@users.noreply.github.com>
2024-07-20 16:51:17 +0500

    Update error_msg_type.py
    class for handling localization

--- strings/error_msg_type.py
+++ strings/error_msg_type.py
@@ -1,28 +1,29 @@
-msgs = {
-    "1": {
-        "en": "User with this email already exists"
-    }
-}
-
 from enums.language import Language
 
-class ResponseString:
-    def __init__(self,headers=None):
-        self.translation_strings=msgs
+class LocalizedResponseHandler:
+    def __init__(self, headers=None):
+        self.translation_strings = msgs
+        self.Languages = Language
         if headers is None or headers.get("locale") is None:
-            self.current_locale = Language.ENGLISH
+            self.current_locale = self.Languages.ENGLISH
         else:
-            self.current_locale=headers.
+            self.current_locale = headers.get("locale")
+    
+    def is_language_supported(self):
+        return self.current_locale in self.Languages
+
+    def getString(self, string_key):
+        try:
+            msg = self.translation_strings[string_key][self.current_locale]
+        except KeyError as e:
+            raise KeyError(f"{string_key} does not exist in {self.current_locale}") from e
+        return msg
+
+ 
 
 
 
-    @staticmethod
-    def is_language_supported():
 
 
 
-
-
-
-    def get_response_string(self):
 
